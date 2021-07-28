d5b50d0a3e6c8c2dff54e7328f198c1b603df54e
iiconic3000 <85829551+iiconic3000@users.noreply.github.com>
2021-07-29 02:36:42 +0530

    Add files via upload
     A drawing with the help of python code( turtle library)

---
+++ turtie.py
@@ -0,0 +1,114 @@
+import turtle                               #importing turtle library
+rengoku = turtle.Screen()                   #creat a graphic window
+rengoku.bgcolor("hotpink")                  #to set background color
+
+jazz = turtle.Turtle()                      #creat turtle named jazz
+jazz.color("white")                         # turle and there arrow color
+jazz.pensize(3)                             # size of my turtle
+Blanko = turtle.Turtle()
+Blanko.color("black")
+Blanko.pensize(4)
+
+jazz.left(45)                               #turning 45 degree left
+jazz.forward(260)                         #move forward 200 units
+
+jazz.left(140)                           #turning 145 degree left
+jazz.forward(300)                         #move 220 degree forward
+jazz.left(150)                              #turning 150 degree left
+jazz.forward(310)                         #move forward 250 unit
+
+jazz.left(150)                              #turning 150 degree left
+jazz.forward(320)                         #move forward 240 unit
+jazz.left(148)                              #turning 140 degree left
+jazz.forward(350)                          #move forward 230 unit
+
+#for B
+Blanko.left(90)
+Blanko.forward(80)
+Blanko.right(90)
+Blanko.forward(25)
+Blanko.right(65)
+Blanko.forward(25)
+Blanko.right(90)
+Blanko.forward(38)
+Blanko.left(135)
+Blanko.forward(45)
+Blanko.right(105)
+Blanko.forward(40)
+Blanko.right(65)
+Blanko.forward(25)
+
+Blanko.right(170)
+Blanko.forward(50)
+
+
+
+#for L
+Blanko.left(90)
+Blanko.forward(80)
+Blanko.right(180)
+Blanko.forward(80)
+Blanko.left(90)
+Blanko.forward(35)
+
+#for A
+Blanko.left(65)
+Blanko.forward(85)
+Blanko.right(155)
+Blanko.forward(85)
+Blanko.right(155)
+Blanko.forward(45)
+Blanko.right(105)
+Blanko.forward(40)
+
+#for N
+Blanko.right(100)
+Blanko.forward(40)
+Blanko.right(180)
+Blanko.forward(80)
+
+Blanko.right(160)
+Blanko.forward(85)
+Blanko.left(160)
+Blanko.forward(70)
+
+#for K
+Blanko.right(90)
+Blanko.forward(10)
+
+
+Blanko.left(270)
+Blanko.forward(60)
+Blanko.left(180)
+Blanko.forward(30)
+Blanko.right(45)
+Blanko.forward(55)
+Blanko.right(180)
+Blanko.forward(55)
+
+
+
+#for O
+Blanko.left(90)
+Blanko.forward(70)
+Blanko.left(45)
+Blanko.forward(30)
+Blanko.left(45)
+Blanko.forward(20)
+
+
+Blanko.left(45)
+Blanko.forward(50)
+Blanko.left(45)
+Blanko.forward(20)
+Blanko.left(45)
+Blanko.forward(30)
+Blanko.left(45)
+Blanko.forward(20)
+
+Blanko.left(45)
+Blanko.forward(70)
+Blanko.right(90)
+Blanko.forward(250)
+
+rengoku.exitonclick()                   #for exit screen on click
